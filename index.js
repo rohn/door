0585d3cb3994a3a634b824e8126b6a06d423b2a1
rohn <rblake@fastpencil.com>
2015-04-03 15:09:05 -0500

    multiple doors means multiple messages

--- index.js
+++ index.js
@@ -18,14 +18,14 @@ var p = new push({
 
 var msg = config.get('pushover');
 
-var sendMessageTimeout;
+var sendMessageTimeout = [];
 
 var opendoor = function(whichDoor) {
   console.log('Door opened: ' + whichDoor);
   payload.state = 'open';
   msg.message = "The " + whichDoor + " is open.";
 
-  sendMessageTimeout = setTimeout(function() {
+  sendMessageTimeout[whichDoor] = setTimeout(function() {
     p.send(msg, function(err, result) {
       if (err) {
         //do something appropriate
@@ -37,7 +37,7 @@ var opendoor = function(whichDoor) {
 var closedoor = function(whichDoor) {
   console.log('Door closed: ' + whichDoor);
   payload.state = 'closed';
-  clearTimeout(sendMessageTimeout);
+  clearTimeout(sendMessageTimeout[whichDoor]);
 };
 
 var pins = config.get('pins');
